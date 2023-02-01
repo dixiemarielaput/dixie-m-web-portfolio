29e173d6c3f80ddf923478426a3186c72f95f5a2
Dixie Pixie <dixiemarielaput@gmail.com>
2023-02-01 19:50:21 +0800

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+
+    <div className="items-center w-full h-20 px-4 flex justify-center text-white bg-cyan-500">
+    <div className="flex justify-center items-center">
+      <div className="text-center">
+        Copyright @2023 All rights reserved.
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Footer;
